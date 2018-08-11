1c0ea488357a69448c26e187be2c4a666d1a8587
dmytro <godundmytro@gmail.com>
2018-08-11 14:15:34 +0300

    Initial set up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel DHDev Origins
                 </div>
 
                 <div class="links">
--- run.sh
+++ run.sh
@@ -4,10 +4,14 @@ cd /var/www
 composer global require hirak/prestissimo
 composer install
 
+npm install
+
 chmod 777 -R /var/www/storage
 chmod 777 -R /var/www/bootstrap/cache
 
 php artisan key:generate
 php artisan migrate:fresh --seed
+php artisan view:cache
+php artisan route:cache
 
 php-fpm
